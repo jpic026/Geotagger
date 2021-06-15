a478cb66fd257363a4060103609e4a3c12a36f5f
JeffreyPickerin <>
2021-06-15 21:48:24 +0000

    Initial commit

---
+++ Renamer.java
@@ -0,0 +1,6 @@
+/* Jeff - 
+
+Work on this. Main.java actionevent should call this class to rename the photos
+found in /sample-photos/ either to rename teh inputs or create duplicates whichever is easiest. 
+
+*/
